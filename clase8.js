f998f9808a24e3eb505865e0ec2654bef8efdcf4
Fabiano <fabiano1976@gmail.com>
2015-02-12 20:38:56 -0300

    functions acelerar and frenar had been added

--- clase8.js
+++ clase8.js
@@ -1,14 +1,39 @@
 var auto = {marca:"ford", 
 			ano:2014, 
-			dni:31456789
-			vmax:120};
+			dni:31456789};
 
 var auto1 = {marca:"porsche", 
 			ano:1999, 
-			dni:61502297
-			vmax:180};
+			dni:61502297};
 
 var auto2 = {marca:"mazda", 
 			ano:2013, 
-			dni:31456789
-			vmax:160};
+			dni:31456789};
+
+auto.vmax = 30;
+auto1.vmax = 45;
+auto2.vmax = 40;
+
+auto.acelerar = function () {
+	console.log(this.vmax * 2);
+}
+
+auto.frenar = function (){
+	this.vmax = 0;
+}
+
+auto1.acelerar = function () {
+	console.log(this.vmax * 2);
+}
+
+auto1.frenar = function (){
+	this.vmax = 0;
+}
+
+auto2.acelerar = function () {
+	console.log(this.vmax * 2);
+}
+
+auto2.frenar = function (){
+	this.vmax = 0;
+}
