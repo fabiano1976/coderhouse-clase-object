75ff3872b28dca649d75cfd41fa890ad01d8fb97
Fabiano <fabiano1976@gmail.com>
2015-02-12 19:51:44 -0300

    clase8.js added

---
+++ clase8.js
@@ -0,0 +1,11 @@
+var auto = {marca:"ford", 
+			ano:2014, 
+			dni:31456789};
+
+var auto1 = {marca:"porsche", 
+			ano:1999, 
+			dni:61502297};
+
+var auto2 = {marca:"mazda", 
+			ano:2013, 
+			dni:31456789};
