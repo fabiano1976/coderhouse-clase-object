23d2a518d176cdb87c8c0e34170f3fc5d489c109
Fabiano <fabiano1976@gmail.com>
2015-02-12 20:18:09 -0300

    vmax added

--- clase8.js
+++ clase8.js
@@ -1,13 +1,14 @@
 var auto = {marca:"ford", 
 			ano:2014, 
-			dni:31456789};
+			dni:31456789
+			vmax:120};
 
 var auto1 = {marca:"porsche", 
 			ano:1999, 
-			dni:61502297};
+			dni:61502297
+			vmax:180};
 
 var auto2 = {marca:"mazda", 
 			ano:2013, 
-			dni:31456789};
-
-var fabiano = { name:"Fabiano"};
+			dni:31456789
+			vmax:160};
