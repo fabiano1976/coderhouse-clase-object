bc9bc14e9d7606ffad1fa5e73d7986ff73271fc3
Fabiano <fabiano1976@gmail.com>
2015-02-19 18:45:45 -0300

    buscar por dni agregado al codigo

--- clase8.js
+++ clase8.js
@@ -8,7 +8,35 @@ var auto1 = {marca:"porsche",
 
 var auto2 = {marca:"mazda", 
 			ano:2013, 
-			dni:31456789};
+			dni:18296789};
+
+var persona = { nombre: "Juan",
+				dni:61502297} 
+
+var persona1 = {nombre: "Fabiano",
+				dni:18296789}
+
+var persona2 = {nombre: "Maria",
+				dni:31456789}
+
+var autos = [auto, auto1, auto2];
+var personas = [persona, persona1, persona2];
+
+function pordni (doc) {
+	for ( i = autos.length - 1; i >= 0; i--) {
+		if (autos[i].dni === doc) {
+			for ( j = personas.length - 1; j >= 0; j--) {
+				if (autos[i].dni === personas[j].dni) {
+				console.log("El auto " + autos[i].marca + " ano: " + autos[i].ano + " pertenece a " + personas[j].nombre );
+			};
+		};
+	};
+};
+
+};
+
+
+
 
 auto.vmax = 30;
 auto1.vmax = 45;
